931df53d4ccf9ab827769d889037098b7b497be2
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-11-10 13:44:17 +0100

    i386/slaunch: Add support for AMD SKINIT
    Extend slaunch command to work on AMD CPUs and validate input binary to
    reject obviously incorrect ones.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- include/grub/i386/slaunch.h
+++ include/grub/i386/slaunch.h
@@ -22,8 +22,9 @@
 #define GRUB_I386_SLAUNCH_H 1
 
 /* Secure launch platform types. */
-#define SLP_NONE      0
-#define SLP_INTEL_TXT 1
+#define SLP_NONE       0
+#define SLP_INTEL_TXT  1
+#define SLP_AMD_SKINIT 2
 
 /* PCRs used by Secure launch. */
 #define GRUB_SLAUNCH_CODE_PCR 17
